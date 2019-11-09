c4f074415312442df7d8e9c388c02bb84ef63610
xNo010 <yuto018.puddhin7@gmail.com>
2019-11-09 15:25:41 +0900

    問題１

---
+++ 1109(土)/01_TotalCal/TotalCal/TotalCal.cpp
@@ -0,0 +1,56 @@
+﻿#include "TotalCal.h"
+
+int main()
+{
+	short NumberList[10]
+	{
+		4, 6, 3, 10, 3, 6, 25, 60, 2, 100
+	};
+
+	// for文での計算
+	printf("for文計算\n\n");
+	For_Cal(NumberList);
+
+	// whileループでの計算
+	printf("\nwhile文計算\n\n");
+	While_Cal(NumberList);
+
+	// 再帰処理での計算
+	tempPass = 0;
+	printf("\n再帰処理計算\n\n");
+	Recur_Cal(NumberList, 10);
+}
+
+void For_Cal(short List[10])
+{
+	// 合計値格納
+	short temp = 0;
+	for (short i = 0; i < 10; i++)
+	{
+		temp += List[i];
+		printf("現在の合計数値:%d\n", temp);
+	}
+}
+void While_Cal(short List[10])
+{
+	short count = 0;
+	short temp = 0;
+	while (count < 10)
+	{
+		temp += List[count];
+		printf("現在の合計数値:%d\n", temp);
+
+		count++;
+	}
+
+}
+void Recur_Cal(short List[10], short count)
+{
+	tempPass += List[--count];
+	printf("現在の合計数値:%d\n", tempPass);
+
+	if (count > 0)
+	{
+		Recur_Cal(List, count);
+	}
+}
---
+++ 1109(土)/01_TotalCal/TotalCal/TotalCal.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef __TOTALCAL_H__
+#define __TOTALCAL_H__
+
+#include <iostream>
+
+short tempPass;
+
+void For_Cal(short List[10]);
+void While_Cal(short List[10]);
+void Recur_Cal(short List[10], short MaxNum);
+#endif /* __TOTALCAL_H__ */
