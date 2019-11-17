d76f8993ef84d03706bcdb29e6ede3ff5bbece51
xNo010 <yuto018.puddhin7@gmail.com>
2019-11-18 02:43:54 +0900

    ひし形課題

---
+++ 1114~/DiamondRot/DiamondRot/DiamondRot.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+#define MaxSize 7
+
+int main()
+{
+	int32_t Diamond[MaxSize][MaxSize];
+	int32_t first, end;
+
+	for (int32_t i = 0; i < MaxSize; i++)
+	{
+		for (int32_t j = 0; j < MaxSize; j++)
+		{
+			Diamond[i][j] = 0;
+		}
+	}
+
+	first = 2;
+	end =	4;
+
+	while (first >= 0 || end < MaxSize)
+	{
+		for (int32_t i = 0; i < MaxSize; i++)
+		{
+			for (int32_t j = 0; j < MaxSize; j++)
+			{
+				if (Diamond[i][j] == 0)
+				{
+					printf("□");
+				}
+				else if (Diamond[i][j] == 1)
+				{
+					printf("■");
+				}
+			}
+			printf("\n");
+
+			if (i == MaxSize - 1)
+			{
+				printf("\n");
+			}
+		}
+
+		for (int32_t i = first; i < end; i++)
+		{
+			Diamond[i][i + 1] = 1;
+		}
+		for (int32_t i = end; i > first; i--)
+		{
+			Diamond[i][i - 1] = 1;
+		}
+
+		first--;
+		end++;
+
+		Sleep(2000);
+	}
+}
