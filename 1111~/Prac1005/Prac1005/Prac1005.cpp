caaf7c8ed421626039431ddf4c618d115b6cfdb8
xNo010 <yuto018.puddhin7@gmail.com>
2019-11-18 00:09:21 +0900

    練習問題/更新

---
+++ 1111~/Prac1005/Prac1005/Prac1005.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+struct GAKUSEI
+{
+	char name[20];
+	int32_t TestPoint;
+	char result;
+};
+
+void ResultDeter(GAKUSEI* pGaku)
+{
+	if (pGaku->TestPoint >= 80)
+	{
+		pGaku->result = 'A';
+	}
+	else if (pGaku->TestPoint >= 70)
+	{
+		pGaku->result = 'B';
+	}
+	else if (pGaku->TestPoint >= 60)
+	{
+		pGaku->result = 'C';
+	}
+	else
+	{
+		pGaku->result = 'D';
+	}
+}
+
+int main()
+{
+	GAKUSEI gaku[4];
+
+	for (int32_t i = 0; i < 4; i++)
+	{
+		printf("%d人目の名前入力\n", i + 1);
+		scanf_s("%20s", &gaku[i].name, 20);
+		printf("%sの得点入力\n", gaku[i].name);
+		scanf_s("%d", &gaku[i].TestPoint);
+
+		ResultDeter(&gaku[i]);
+	}
+
+	for (int32_t i = 0; i < 4; i++)
+	{
+		printf("%s, %d, %c\n", gaku[i].name, gaku[i].TestPoint, gaku[i].result);
+	}
+}
---
+++ 1111~/Prac1006/Prac1006/Prac1006.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+struct POSITION
+{
+	double latitude;	// 緯度
+	double longitude;	// 経度
+};
+
+struct CITYINFO
+{
+	char name[50];
+	struct POSITION pos;
+};
+
+void ShowCityInfo(struct CITYINFO* aCity, int count)
+{
+	for (int32_t i = 0; i < count; i++)
+	{
+		printf("%s、緯度:%2.6f、経度:%2.6f\n", aCity[i].name, aCity[i].pos.latitude, aCity[i].pos.longitude);
+	}
+}
+
+int main()
+{
+	struct CITYINFO aCity[] =
+	{
+		{"東京", 35.41, 139.45},
+		{"大阪", 34.41, 135.29},
+		{"名古屋", 35.11, 136.54},
+	};
+
+	ShowCityInfo(aCity, 3);
+}
